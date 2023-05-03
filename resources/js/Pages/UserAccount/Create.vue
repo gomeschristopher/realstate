47dd15119b7580c20fe96bb4cd215c6ddd688a6a
Christopher Gomes <gomeschristopher@hotmail.com>
2023-05-03 06:58:37 -0300

    authentication

---
+++ resources/js/Pages/UserAccount/Create.vue
@@ -0,0 +1,45 @@
+<template>
+    <form @submit.prevent="register">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label class="label" for="name">Your Name</label>
+                <input type="text" id="name" class="input" v-model="form.name" />
+                <div class="input-error" v-if="form.errors.name">{{ form.errors.name }}</div>
+            </div>
+            <div class="mt-4">
+                <label class="label" for="email">E-mail</label>
+                <input type="text" id="email" class="input" v-model="form.email" />
+                <div class="input-error" v-if="form.errors.email">{{ form.errors.email }}</div>
+            </div>
+            <div class="mt-4">
+                <label class="label" for="password">Password</label>
+                <input type="password" id="password" class="input" v-model="form.password" />
+                <div class="input-error" v-if="form.errors.password">{{ form.errors.password }}</div>
+            </div>
+            <div class="mt-4">
+                <label class="label" for="password_confirmation">Confirm Password</label>
+                <input type="password" id="password_confirmation" class="input" v-model="form.password_confirmation" />
+            </div>
+            <div class="mt-4">
+                <button class="btn-primary w-full" type="submit">Create Account</button>
+
+                <div class="mt-2 text-center">
+                    <Link :href="route('login')" class="text-sm text-gray-500">Already heave an account? Click here</Link>
+                </div>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { useForm, Link } from '@inertiajs/vue3';
+
+    const form = useForm({
+        name: null,
+        email: null,
+        password: null,
+        password_confirmation: null
+    });
+
+    const register = () => form.post(route('user-account.store'));
+</script>
